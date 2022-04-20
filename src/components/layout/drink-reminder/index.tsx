8fc8661669b3fa3958727b0a64f42928612915ed
Dima <bycodexp@gmail.com>
2022-04-20 11:06:42 +0200

    add animation

--- src/components/layout/drink-reminder/index.tsx
+++ src/components/layout/drink-reminder/index.tsx
@@ -15,7 +15,7 @@ const DrinkReminder: FC = () => {
       <div className="flex justify-between mt-2">
          <span className="text-slate-500 leading-8 text-xs">It's water time {":)"}</span>
          <button onClick={click} className="bg-sky-50 border-sky-400 border w-8 h-8 rounded">
-            <FontAwesomeIcon icon={faDroplet} className="text-blue-400" />
+            <FontAwesomeIcon icon={faDroplet} className="animate-scale text-blue-400" />
          </button>
       </div>
    );
